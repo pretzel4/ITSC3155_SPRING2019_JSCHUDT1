9bfd927e43edd1200203f07efded8b23d40f5265
beetlejuice <joshuaschudt@gmail.com>
2021-02-21 16:24:15 -0500

    PythonBasics 2 - added logic to count_threes function

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -11,9 +11,12 @@
 # to n (including n).
 
 def count_threes(n):
-  # YOUR CODE HERE
-
-  return
+  # YOUR CODE HERE'
+  count = 0
+  for i in range(n + 1):
+    if(i % 3 == 0 and i != 0):
+      count+=1
+  return count
 
 
 # Part B. longest_consecutive_repeating_char
